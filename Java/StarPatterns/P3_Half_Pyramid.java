cdb93e466d5648fed8716e21d53aee64027962b7
shubhamkute123 <161607312+shubhamkute123@users.noreply.github.com>
2026-01-09 17:45:04 +0530

    Add half pyramid pattern

---
+++ Java/StarPatterns/P3_Half_Pyramid.java
@@ -0,0 +1,23 @@
+// Program to print a half pyramid star pattern
+public class P3_Half_Pyramid {
+    public static void main(String args[]) {
+      
+        int n = 4;
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+/*
+Output:
+*
+**
+***
+****
+*/
