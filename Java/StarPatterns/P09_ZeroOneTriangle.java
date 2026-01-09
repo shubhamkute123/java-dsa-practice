f97888996efbbe02ba2fe04ff47c1d1f721ebf4f
shubhamkute123 <161607312+shubhamkute123@users.noreply.github.com>
2026-01-09 18:43:06 +0530

    Add 0-1 triangle pattern

---
+++ Java/StarPatterns/P09_ZeroOneTriangle.java
@@ -0,0 +1,29 @@
+// Program to print a 0-1 triangle pattern
+public class P09_ZeroOneTriangle {
+    public static void main(String args[]) {
+
+        int n = 5;
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                int sum = i+j;
+                if(sum%2==0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+/*
+Output:
+1 
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+*/
