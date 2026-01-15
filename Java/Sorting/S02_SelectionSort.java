6299c53e34e27079c10f12dad06c9846d8570d79
shubhamkute123 <161607312+shubhamkute123@users.noreply.github.com>
2026-01-15 08:47:18 +0530

    Improve documentation: include all time complexity cases for sorting algorithms

--- Java/Sorting/S02_SelectionSort.java
+++ Java/Sorting/S02_SelectionSort.java
@@ -2,7 +2,7 @@
 public class S02_SelectionSort {
     public static void main(String[] args) {
 
-        System.out.println("Selection Sort...\n");
+        System.out.println("Selection Sort...");
         int[] arr = {80, 30, 40, 10, 70};
         int n = arr.length;
         int smallest, temp;
@@ -28,9 +28,16 @@ public class S02_SelectionSort {
 }
 
 /*
-time complexity = O(n^2)
+Time Complexity:
+Best Case    : O(n^2)
+Average Case : O(n^2)
+Worst Case   : O(n^2)
+
+Space Complexity:
+O(1)
+
 Output:
 Selection Sort...
-
-10 30 40 70 80 
+10 30 40 70 80
 */
+
